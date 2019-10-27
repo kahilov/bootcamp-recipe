185b6734e8e5b22109a2972170a9557cf521539b
Ahmed <kahildude@gmail.com>
2019-10-27 11:18:09 +0200

    first commit

---
+++ server/routes/api.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const request = require('request')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const api = require('./routes/api')
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname,"..", 'dist')))
+app.use(express.static(path.join(__dirname,"..", 'node_modules')))
+app.use('/', api)
+
+
+
+
+
+
+
+
+app.listen(3000, () => console.log(`Server is up and running smoothly`))
